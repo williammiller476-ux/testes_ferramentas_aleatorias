1b476d1d88b6490c911c680e4a7c07606dd9ab7e
williammiller476-ux <williammiller476@gmail.com>
2025-12-09 12:23:42 -0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,9 @@
 document.getElementById('startBtn').addEventListener('click', async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: () => {
-        // Envia uma mensagem para o content.js iniciar
-        window.postMessage({ type: "INICIAR_AUTOMACAO" }, "*");
-    }
-  });
+  chrome.tabs.sendMessage(tab.id, { type: "INICIAR_AUTOMACAO" });
+});
+
+document.getElementById('stopBtn').addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.tabs.sendMessage(tab.id, { type: "PARAR_AUTOMACAO" });
 });
